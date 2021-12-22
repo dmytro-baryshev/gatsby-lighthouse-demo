0d6e3aec58472eb3f39af14d87086bb4e4864d98
Dmitriy Baryshev <dbaryshev13@gmail.com>
2021-12-22 13:05:34 +0200

    add page content loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [loading, setLoading] = useState(true);
+  const [content, setContent] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => {
+          setContent(json);
+          setLoading(false);
+        })
+    }, 3000);
+  }, []);
+
+  if (loading) {
+    return <div>Loading!</div>
+  }
+
+  return <div>{JSON.stringify(content)}</div>
 }
