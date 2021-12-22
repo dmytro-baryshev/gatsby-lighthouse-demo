b26e35994824710fa58f873d5dff7894d338da13
Dmitriy Baryshev <dbaryshev13@gmail.com>
2021-12-22 14:57:53 +0200

    add lighthouse assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,5 +3,16 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    "assert": {
+      "preset": "lighthouse:no-pwa",
+      "assertions": {
+        "categories:performance": ["warn", { "minScore": 0.9 }],
+        "categories:accessibility": ["error", { "minScore": 1 }],
+        "dom-size": ["error", { "maxNumericValue": 3000 }],
+        "offscreen-images": "off",
+        "color-contrast": "off",
+        "tap-targets": "off"
+      }
+    }
   },
 };
