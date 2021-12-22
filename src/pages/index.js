5cc1b86b61108897d03f216a9fa606323734c32c
Dmitriy Baryshev <dbaryshev13@gmail.com>
2021-12-22 14:57:53 +0200

    add lighthouse assertions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,38 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json)
+          setPosts(json);
+          setLoading(false);
+        });
+    }, 5000);
+  }, []);
+
+  if (loading) {
+    return <Loader
+      type="TailSpin"
+      color="#00BFFF"
+      height={100}
+      width={100}
+    />
+  }
+
+  return <div style={{ display: 'grid' }}>{posts.map((post) => (
+    <div key={post.id}>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <h4>By user №{post.userId}</h4>
+    </div>
+  ))}</div>
 }
